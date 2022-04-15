5e5d42234c5a1385577dee8aaf885496a19cfb3d
popacatalin <catalinpopa529@yahoo.com>
2022-04-15 11:21:34 +0300

    create components

---
+++ create-components/README.md
@@ -0,0 +1,11 @@
+# CreateComponents
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.1.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+## Create components
+
+Run `ng g c` or `ng generate component` to create a new component. You can customizing after your like.
---
+++ create-components/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FirstComponentComponent} from "./first-component/first-component.component";
+import {SecondComponentComponent} from "./second-component/second-component.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: 'create-components', pathMatch: 'full'},
+  {path: 'firstComponent', component: FirstComponentComponent},
+  {path: 'secondComponent', component: SecondComponentComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
